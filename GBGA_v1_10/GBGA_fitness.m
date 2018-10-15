d9dfb8c1e56410657ae61092cdbeed8a269a7b15
DESKTOP-A5FBOJD\pppp8 <knight16729438@gmail.com>
2018-10-15 10:30:35 +0800

    Grid Based GA - Obstacle Import Setup

---
+++ GBGA_v1_10/GBGA_fitness.m
@@ -0,0 +1,37 @@
+%
+% Copyright (c) 2015, Yarpiz (www.yarpiz.com)
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Code: YPEA101
+% Project Title: Implementation of Binary Genetic Algorithm in MATLAB
+% Publisher: Yarpiz (www.yarpiz.com)
+% 
+% Developer: S. Mostapha Kalami Heris (Member of Yarpiz Team)
+% 
+% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
+%
+
+function sums=GBGA_fitness(x, gs)
+    
+    ones = find(x);
+    
+    sums = 0;
+    for i = 1: numel(ones)
+        posi = GBGA_getGridPos(ones(i), gs);
+        for j = 1: numel(ones)
+            if (i ~= j)
+                posj = GBGA_getGridPos(ones(j), gs);
+                sums = sums + GBGA_getPosDiff(posi, posj);
+            end
+        end
+    end
+
+    if (numel(ones) < 5)
+        %sums = sums +  (5-numel(ones)) * 30;
+    end
+    if x(1) ~= 1 || x(25) ~= 1
+       % sums = sums + 150;
+    end
+    
+    z=sum(x);
+end
