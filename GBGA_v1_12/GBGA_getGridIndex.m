f6ae3f1dbfb4d63f6e80ddc0ad22313057b7d00f
DESKTOP-A5FBOJD\pppp8 <knight16729438@gmail.com>
2018-10-16 12:09:36 +0800

    Revert "Grid Based GA - A* Search Algorithm for Fitness Implementation"
    This reverts commit 507520d8d5528a048795bf2b146d4753e7954ee6.

--- GBGA_v1_12/GBGA_getGridIndex.m
+++
@@ -1,3 +0,0 @@
-function idx = GBGA_getGridIndex(pos, gs)
-    idx = (pos(2)-1)*gs(1) + pos(1);
-end
--- GBGA_v1_20/GBGA_getPosDiff.m
+++
@@ -1,3 +0,0 @@
-function posDiff = GBGA_getPosDiff(pos1, pos2)
-    posDiff = norm(pos1 - pos2);
-end
